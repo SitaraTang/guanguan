976ac2c4df0d298acad67a45360d861901dbeb30
wing_vvv <36961010+wyl2006bj@users.noreply.github.com>
2019-07-17 15:29:51 +0800

    fix RouteDao - wyl

--- ustbtravel/ustbtravel/src/main/java/edu/ustb/dao/RouteDao.java
+++ ustbtravel/ustbtravel/src/main/java/edu/ustb/dao/RouteDao.java
@@ -9,5 +9,5 @@ public interface RouteDao {
 	public int findTotalCount(int cid);
 	public List<Route> findByPage(int cid,int start,int pagesize);
 	public int findTotalCount(int cid,String rname);
-	public List<Route> findByPage(int cid,String start,int pageSize,String rname)
+	public List<Route> findByPage(int cid,String start,int pageSize,String rname);
 }
